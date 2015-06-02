127769716cad16cb1eed0765c3334beef35d7426
Roger Jose Padilla Camacho <rogerjose81@gmail.com>
2015-06-02 01:09:03 -0500

    Karma browsers

--- karma.conf.js
+++ karma.conf.js
@@ -25,10 +25,10 @@ module.exports = function (config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
-//      'Chrome',
-//      'Firefox',
-//      'IE',
-//      'IE9',
+     'Chrome',
+     'Firefox',
+     'IE',
+     'IE9',
       'PhantomJS'
     ],
 
