8ad440ad4fcb5d2b9d7a5bf3fe65a183e7e5b98a
Roger Padilla <rpadilla@velocityparnerts.net>
2017-03-26 14:31:17 -0500

    migrate from gulp to webpack

--- karma.conf.js
+++ karma.conf.js
@@ -1,71 +1,96 @@
-// Note some browser launchers should be installed before using karma start.
-// For example:
-// npm install karma-firefox-launcher
-// karma start --browsers=Firefox
+var testWebpackConfig = require('./config/webpack.test.js');
+
 module.exports = function (config) {
-  config.set({
 
-    frameworks: ['mocha', 'chai'],
+  var configuration = {
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    // base path that will be used to resolve all patterns (e.g. files, exclude)
+    basePath: '',
+
+    /*
+     * Frameworks to use
+     *
+     * available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+     */
+    frameworks: ['jasmine'],
+
+    // list of files to exclude
+    exclude: [],
+
+    /*
+     * list of files / patterns to load in the browser
+     *
+     * we are building the test environment in ./spec-bundle.js
+     */
+    files: [{ pattern: './config/spec-bundle.js', watched: false }],
+
+    /*
+     * preprocess matching files before serving them to the browser
+     * available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+     */
+    preprocessors: { './config/spec-bundle.js': ['coverage', 'webpack', 'sourcemap'] },
+
+    // Webpack Config at ./webpack.test.js
+    webpack: testWebpackConfig,
+
+    coverageReporter: {
+      type: 'in-memory'
+    },
+
+    remapCoverageReporter: {
+      'text-summary': null,
+      json: './reports/coverage/coverage.json',
+      html: './reports/coverage/html'
+    },
+
+    bambooReporter: {
+      filename: 'reports/mocha.json'
+    },
+
+    // Webpack please don't spam the console when running in karma!
+    webpackMiddleware: { stats: 'errors-only' },
+
+    /*
+     * test results reporter to use
+     *
+     * possible values: 'dots', 'progress'
+     * available reporters: https://npmjs.org/browse/keyword/karma-reporter
+     */
+    reporters: [
+      'mocha',
+      'coverage',
+      'remap-coverage'
+    ],
+
+    // web server port
+    port: 9876,
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    /*
+     * level of logging
+     * possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+     */
     logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    /*
+     * start these browsers
+     * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+     */
     browsers: [
-      // 'Chrome',
-      // 'Firefox',
-      // 'IE',
-      // 'IE9',
       'PhantomJS'
     ],
 
-    // you can define custom flags
-    customLaunchers: {
-      IE9: {
-        base: 'IE',
-        'x-ua-compatible': 'IE=EmulateIE9'
-      }
-    },
+    /*
+     * Continuous Integration mode
+     * if true, Karma captures browsers, runs the tests and exits
+     */
+    singleRun: true
+  };
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    files: [
-      'test/setup/*.js',
-      'src/prouter.js',
-      'test/*.spec.js'
-    ],
-
-    preprocessors: {
-      'src/prouter.js': ['coverage']
-    },
-
-    coverageReporter: {
-      dir: 'build/reports/coverage',
-      reporters: [
-        // reporters not supporting the `file` property
-        {
-          type: 'html',
-          subdir: 'html'
-        }, {
-          type: 'lcov',
-          subdir: 'lcov'
-        }
-      ]
-    }
-  });
+  config.set(configuration);
 };
--- test/setup/dom-setup.js
+++
@@ -1,6 +0,0 @@
-document.body.insertAdjacentHTML('beforeend',
-    '<div id="qunit"></div>' +
-    '<div id="qunit-fixture">' +
-        '<div id="testElement"><h1>Test</h1></div>' +
-    '</div>'
-);
