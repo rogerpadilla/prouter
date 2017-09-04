4c530d51ebfa40becc967538c825657556eeaaee
Roger Padilla <rpadilla@velocityparnerts.net>
2017-09-04 07:49:49 -0500

    added "types" proyecty to tsconfig

--- dist/router.d.ts
+++
@@ -1,9 +0,0 @@
-import { RequestCallback, Options } from './';
-export declare abstract class Router {
-    private opts;
-    private handlers;
-    private res;
-    constructor(opts: Options);
-    use(path: string, callback: RequestCallback): this;
-    protected processPath(path: string): void;
-}
