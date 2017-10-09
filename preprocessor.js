157fa04b8b98c6924343bf6e94d2423a88521a04
Roger Padilla <rpadilla@velocityparnerts.net>
2017-10-09 15:48:18 -0500

    tsConfig.compilerOptions.module = 'commonjs'; for testing

--- preprocessor.js
+++ preprocessor.js
@@ -1,6 +1,8 @@
 const tsc = require('typescript');
 const tsConfig = require('./tsconfig.json');
 
+tsConfig.compilerOptions.module = 'commonjs';
+
 module.exports = {
   process(src, path) {
     if (path.endsWith('.ts')) {
