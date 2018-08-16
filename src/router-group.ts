36601ad4b51a69f41c8e86f8ce20cc9541f0c4a9
Roger Padilla <rpc@Rogers-MacBook-Pro.local>
2018-08-16 08:58:49 -0500

    simplified ProuterGroup

--- src/router-group.ts
+++ src/router-group.ts
@@ -3,14 +3,12 @@ import { ProuterRequestCallback, ProuterGroup } from './entity';
 
 export function buildRouterGroup() {
 
-  const handlers: { path: string, callback: ProuterRequestCallback }[] = [];
-
   const groupObj: ProuterGroup = {
 
-    handlers,
+    handlers: [],
 
     use(path: string, callback: ProuterRequestCallback) {
-      handlers.push({ path, callback });
+      groupObj.handlers.push({ path, callback });
       return groupObj;
     }
 
