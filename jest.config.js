9d182c2ecb24ddd1ac5294c1ee6c34fe8f355163
Roger Padilla <rpadilla@velocitypartners.net>
2018-08-16 11:11:49 -0500

    upgraded ts-jest and externalized jest's config

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testMatch": ["**/*.spec.ts"],
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "collectCoverage": true,
+  "coverageReporters": [
+    "html",
+    "lcov"
+  ],
+  "coverageDirectory": "coverage",
+};
